b258b2d82beca125c43df0094943c83324fdb96b
Sajal Rajabhoj <sajalrajabhoj@gmail.com>
2020-04-05 23:40:17 +0530

    removed car.ts

--- src/app/gameservice.ts
+++ src/app/gameservice.ts
@@ -1,13 +1,23 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
-import { Car } from "./car";
 
 @Injectable()
 export class GameService {
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Content-Type": "application/json"
+    })
+  };
+
   constructor(private http: HttpClient) {}
 
   getGames() {
-    return this.http.get("http://starlord.hackerearth.com/TopSellingGames");
+    return this.http.get(
+      "https://starlord.hackerearth.com/TopSellingGames",
+      this.httpOptions
+    );
   }
 }
